f4c4391465877ce6a04ca7c287012ccc6438bbd9
Sebastian Slomski <sebastian@purelabs.io>
2015-06-26 08:40:15 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-classnames-tag',
-    version='0.1',
+    version='0.2',
     include_package_data=True,
     packages=['django_classnames_tag'],
     url='http://github.com/sebslomski/django-classnames-tag',
